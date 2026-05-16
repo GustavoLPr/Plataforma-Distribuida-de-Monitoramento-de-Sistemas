5ededaa5f90d66012695ecc5df20078dc45c3f43
Gustavo Leal <gustavolealprado@gmail.com>
2026-05-16 17:48:22 -0300

    feat: API funcional

--- API/API/src/main/java/com/monitoramento/API/dto/maquina/MaquinaCadastroDto.java
+++ API/API/src/main/java/com/monitoramento/API/dto/maquina/MaquinaCadastroDto.java
@@ -3,6 +3,7 @@ package com.monitoramento.API.dto.maquina;
 import jakarta.validation.constraints.NotBlank;
 
 public record MaquinaCadastroDto(
+    Long id,
     @NotBlank String nome,
     @NotBlank String ip
 ) {
